91de3593d23bdcea350d13bfc11ca1b091ee12b3
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2016-09-22 20:17:50 +0300

    .

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application home page..
  */
 @Controller
 public class HomeController {
